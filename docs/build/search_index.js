4c5335895944bd7be6eab408527286db1122386a
stla <laurent_step@yahoo.fr>
2020-12-14 20:10:47 +0100

    gh-pages

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#Jack.jl-documentation","page":"Documentation","title":"Jack.jl documentation","text":"","category":"section"},{"location":"index.html#Member-functions","page":"Documentation","title":"Member functions","text":"","category":"section"},{"location":"index.html","page":"Documentation","title":"Documentation","text":"Modules = [Jack]\nOrder   = [:type, :function]","category":"page"}]
+}
