fed2136a543bbbfb918dfe61f728835e8ac51cd7
stla <laurent_step@yahoo.fr>
2020-12-14 19:51:16 +0100

    docs make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,23 @@
-using Documenter, Jack
+push!(LOAD_PATH,"../src/")
 
-makedocs(sitename="Jack documentation")
+using Documenter
+using Jack
 
-deploydocs(
-    repo = "github.com/stla/Jack.jl.git",
+makedocs(
+    sitename = "Jack.jl",
+    authors = "Stéphane Laurent",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://stla.github.io/Jack.jl",
+        assets = String[],
+    ),
+    modules = [Jack],
+    pages = ["Documentation"  => "index.md"],
+    repo = "https://github.com/stla/Jack.jl/blob/{commit}{path}#{line}"
+)
+
+deploydocs(;
+    branch = "gh-pages",
+    devbranch = "main",
+    repo = "github.com/stla/Jack.jl",
 )
